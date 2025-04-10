a22f79a2309ab3423a750e298d5abdb1f0a4a18a
Sahil Shefeek <sahilms345@gmail.com>
2025-04-10 21:17:14 +0530

    Add emotion analysis endpoint and session videos view

--- therapy_sessions/urls.py
+++ therapy_sessions/urls.py
@@ -1,10 +1,17 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import SessionViewSet
+from rest_framework_nested import routers
+
+from therapy_sessions.views import SessionViewSet, SessionVideosView
 
 router = DefaultRouter()
-router.register(r'', SessionViewSet)
+router.register('', SessionViewSet, basename='sessions')
+
+# Add nested route for session videos
+session_router = routers.NestedSimpleRouter(router, r'', lookup='session')
 
 urlpatterns = [
     path('', include(router.urls)),
+    # Add path for emotion analysis
+    path('<int:session_pk>/analysis/emotions/', SessionVideosView.as_view(), name='session-emotion-analysis'),
 ]
